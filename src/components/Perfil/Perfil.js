e68befd5c6d55c84db4dca860178a6e649ca8b08
Lucas Vilar <lucas.vilar360@gmail.com>
2023-06-26 15:40:36 -0300

    Modal de Editar Perfil

--- src/components/Perfil/Perfil.js
+++ src/components/Perfil/Perfil.js
@@ -1,20 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Perfil.css';
+import { FaTimes, FaCamera } from 'react-icons/fa';
+import { FiUpload } from 'react-icons/fi';
 
-const Perfil = ({ imageSrc, name, username, text, imageSrcTweet, timeElapsed }) => {
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
+
+const Perfil = ({ imageSrc, name, username }) => {
+
+    const [isEditPerfilModalOpen, setIsEditPerfilModalOpen] = useState(false);
+
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [imagePreview, setImagePreview] = useState('');
+
+    const openEditPerfilModal = () => {
+        setIsEditPerfilModalOpen(true);
+    }
+
+    const closeEditPerfilModal = () => {
+        setIsEditPerfilModalOpen(false);
+    }
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        setSelectedImage(file);
+        const previewURL = URL.createObjectURL(file);
+        setImagePreview(previewURL);
+    };
+
+    const handleImageRemove = () => {
+        setSelectedImage(null);
+        setImagePreview('');
+    };
+
     return (
         <div className="Perfil">
             <div className='div-background-perfil'></div>
             <div className="perfil-avatar-container">
-            
+
                 <img src={imageSrc} className="perfil-avatar" />
-                
-                
+
+
             </div>
-           <h2 className='perfil-h2'>{name}</h2>
-           <a className="perfil-username">{username}</a>
-           <button className='editar-meuperfil'>Editar</button>
-           
+            <h2 className='perfil-h2'>{name}</h2>
+            <a className="perfil-username">{username}</a>
+            <button className='editar-meuperfil' onClick={openEditPerfilModal}>Editar</button>
+            <Modal
+                isOpen={isEditPerfilModalOpen}
+                onRequestClose={closeEditPerfilModal}
+                className="modal-login"
+                overlayClassName="overlay-login"
+            >
+                <div className='div-cadastro'>
+
+                    <button className='modal-close-button' onClick={closeEditPerfilModal}><FaTimes /></button>
+                    <h2 className="modal-login__title">Editar perfil</h2>
+                    <form className='modal-login__form'>
+
+
+                        <div className="form__group field">
+                            <input
+                                type="input"
+                                className="form__field"
+                                placeholder="Email"
+                                id="name-edit-perfil"
+                                // onChange={handleEmailLoginChange}
+                                // value={emailLogin}
+                                style={{ marginBottom: '20px' }}
+                            />
+                            <label htmlFor="name-edit-perfil" className="form__label">Nome</label>
+                        </div>
+                        <h2 className='foto-de-perfil-label'>Foto de perfil</h2>
+                        {selectedImage ? (
+                            <div className='div-imagem-remove'>
+                                {/* <p>Imagem selecionada: {selectedImage.name}</p> */}
+
+                                <img src={imagePreview} alt="Imagem selecionada" className="image-preview-edit-perfil" />
+                                <button className='remove-image-button' onClick={handleImageRemove}><FaTimes className='fa-times-remove-image' /></button>
+
+                            </div>
+                        ) : (
+                            // <label htmlFor="tweet-image" className="upload-button">
+                            //     <FiUpload className="upload-icon" />
+                            //     Adicionar imagem
+                            //     <input
+                            //         type="file"
+                            //         id="tweet-image"
+                            //         accept="image/*"
+                            //         className="upload-input"
+                            //         onChange={handleImageChange}
+                            //     />
+                            // </label>
+                            <div className="circular-input">
+
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    className="input"
+                                    onChange={handleImageChange}
+                                    id="file-input-editperfil"
+                                />
+                                <label htmlFor="file-input-editperfil">
+                                    <div className="icon-container">
+                                        <FaCamera className="icon" />
+                                    </div>
+                                </label>
+                            </div>
+
+                        )}
+
+
+                       
+                        <button
+                            type="submit"
+                            className='modal-login__button'
+                        // onClick={handleTweetSubmit}
+                        >
+                            Salvar</button>
+                    </form>
+
+
+                </div>
+
+            </Modal>
+
 
         </div>
 
